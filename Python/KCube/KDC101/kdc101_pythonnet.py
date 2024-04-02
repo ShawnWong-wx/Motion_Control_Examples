b54be9da7c87d396883bd98b461a8b5e2866eca0
Shawn Wong <xwang3@email.arizona.edu>
2024-04-01 17:59:18 -0700

    update

--- Python/KCube/KDC101/kdc101_pythonnet.py
+++ Python/KCube/KDC101/kdc101_pythonnet.py
@@ -1,11 +1,18 @@
 """An example that uses the .NET Kinesis Libraries to connect to a KDC."""
+
 import os
 import time
 import clr
 
-clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.DeviceManagerCLI.dll")
-clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.GenericMotorCLI.dll")
-clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\ThorLabs.MotionControl.KCube.DCServoCLI.dll")
+clr.AddReference(
+    "C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.DeviceManagerCLI.dll"
+)
+clr.AddReference(
+    "C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.GenericMotorCLI.dll"
+)
+clr.AddReference(
+    "C:\\Program Files\\Thorlabs\\Kinesis\\ThorLabs.MotionControl.KCube.DCServoCLI.dll"
+)
 from Thorlabs.MotionControl.DeviceManagerCLI import *
 from Thorlabs.MotionControl.GenericMotorCLI import *
 from Thorlabs.MotionControl.KCube.DCServoCLI import *
@@ -30,7 +37,9 @@ def main():
         device.Connect(serial_no)
         time.sleep(0.25)
         device.StartPolling(250)
-        time.sleep(0.25)  # wait statements are important to allow settings to be sent to the device
+        time.sleep(
+            0.25
+        )  # wait statements are important to allow settings to be sent to the device
 
         device.EnableDevice()
         time.sleep(0.25)  # Wait for device to enable
@@ -45,10 +54,11 @@ def main():
             assert device.IsSettingsInitialized() is True
 
         # Before homing or moving device, ensure the motor's configuration is loaded
-        m_config = device.LoadMotorConfiguration(serial_no,
-                                                DeviceConfiguration.DeviceSettingsUseOptionType.UseFileSettings)
+        m_config = device.LoadMotorConfiguration(
+            serial_no, DeviceConfiguration.DeviceSettingsUseOptionType.UseFileSettings
+        )
 
-        m_config.DeviceSettingsName = "PRMTZ8"
+        m_config.DeviceSettingsName = "MTS50-Z8"
 
         m_config.UpdateCurrentConfiguration()
 
@@ -57,13 +67,13 @@ def main():
         print("Homing Actuator")
         device.Home(60000)  # 10s timeout, blocking call
 
-        f = 50.0
+        f = 10.0
         d = Decimal(f)
-        print(f'Device Homed. Moving to position {f}')
+        print(f"Device Homed. Moving to position {f}")
         device.MoveTo(d, 20000)  # 10s timeout again
         time.sleep(1)
 
-        print(f'Device now at position {device.Position}')
+        print(f"Device now at position {device.Position}")
         time.sleep(1)
 
         device.Disconnect()
